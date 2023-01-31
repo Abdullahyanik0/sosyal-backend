fcb0fac08dad2db627cc76d0b73db8cd088b0916
Abdullahyanik <abdullahyanik016@gmail.com>
2023-01-31 23:24:54 +0300

    add register and login

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 mongoose.set("strictQuery", false);
+
 const connectDB = async () => {
   console.log("bağlandı port");
-
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected", conn.connection.host);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("connected", connect.connection.host);
   } catch (error) {
     console.log(error);
     process.exit(1);
@@ -20,38 +20,77 @@ const connectDB = async () => {
 };
 
 app.get("/", (req, res) => {
-  res.send({ title: "books" });
+  res.send({ title: "hello" });
 });
 
-app.get("/add-note", async (req, res) => {
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  userName: String,
+  confirm: Boolean,
+  created_date: String,
+});
+const User = mongoose.model("User", UserSchema);
+const secretKey = "deneme";
+const tokenExpiresIn = "1h";
+const refreshTokenExpiresIn = "24h";
+
+const middleware = (req, res, next) => {
   try {
-    await Book.insertMany([
-      {
-        title: "kitap 1",
-        required: "body 1",
-      },
-      {
-        title: "kitap 2",
-        required: "body 2",
-      },
-    ]);
-    res.send("add notes");
-  } catch (error) {
-    console.log(error, "errr");
+    const token = req.headers.token;
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    if (err.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Token expired" });
+    } else {
+      return res.status(402).json({ message: "Invalid token" });
+    }
   }
+};
+
+app.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (user) {
+      res.status(417).json({ message: "Kullanıcı zaten kayıtlı." });
+    } else if (err) {
+      res.status(500).json(err);
+    } else {
+      new User({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        userName: req.body.userName,
+        confirm: req.body.confirm,
+        created_date: req.body.created_date,
+      }).save();
+      res.status(201).json({
+        user: User,
+        message: "Kayıt başarılı.",
+      });
+    }
+  });
 });
 
-app.get("/books", async (req, res) => {
-  const book = await Book.find();
-  if (book) {
-    res.json(book);
-  } else {
-    res.send("falan filan");
-  }
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    const password = user.password;
+    if (user && password === req.body.password) {
+      const token = jwt.sign({ id: user._id, email: user.email }, secretKey, { expiresIn: tokenExpiresIn });
+      const refreshtoken = jwt.sign({ id: user._id, email: user.email }, secretKey, { expiresIn: refreshTokenExpiresIn });
+      res.status(201).json({ message: "Giriş Başarılı", token: token, refreshtoken: refreshtoken });
+    } else if (user && password !== req.body.password) {
+      res.status(401).json({ message: "Şifren Yanlış" });
+    } else {
+      res.status(402).json({ message: "Kullanıcı Bulunamadı" });
+    }
+  });
 });
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("bağlandı port");
+    console.log("bağlandı port", PORT);
   });
 });
