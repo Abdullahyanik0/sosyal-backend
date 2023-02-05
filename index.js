e5dc116ebf5db9d7443b06609c68014c4237149b
Abdullahyanik <abdullahyanik016@gmail.com>
2023-02-05 15:56:38 +0300

    add profile service

--- index.js
+++ index.js
@@ -100,16 +100,22 @@ app.post("/login", (req, res) => {
   });
 });
 
-app.get("/user/:email", (req, res) => {
-  console.log(req.params.email);
+app.put("/profile/:id", (req, res) => {
+  const userId = req.params.id;
 
-  User.findOne({ email: req.params.email }, (err, user) => {
-    if (user) {
-      res.status(201).json({ user: user });
-    } else {
-      res.status(402).json({ message: "Kullanıcı Bulunamadı" });
+  User.findOneAndUpdate(
+    { _id: userId },
+    { $set: { name: req.body.name, email: req.body.email, userName: req.body.userName, password: req.body.password } },
+    { returnOriginal: false },
+    (err, result) => {
+      if (result) {
+        return res.status(200).json({ message: "Başarılı" });
+      }
+      if (err) {
+        return res.status(401).json({ message: "Hata" });
+      }
     }
-  });
+  );
 });
 
 connectDB().then(() => {
